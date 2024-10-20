aeb1fb380923dd4bba8edc509707fc991d638004
TheOwlCoder <100481826+TheOwlCoder@users.noreply.github.com>
2024-10-20 16:55:02 -0500

    Create index.js

---
+++ sideload example/index.js
@@ -0,0 +1,10 @@
+if (window.location.href == "https://wasteof.money/settings") {
+  addMenuDropdown("plugins", "Sideload Plugin Example", "your-id-here");
+  addHeaderNode("your-id-here-content", "Example Title");
+  addTextNode("your-id-here-content", "Example Description");
+  addTextNode("your-id-here-content", "<b>By</b> You!");
+  addTextNode("your-id-here-content", "<b>Version</b> 1.0");
+  addButtonNode("your-id-here-content", "Enable", "your-id-hereonoff")
+
+  pluginEnableDisable("your-id-here");
+}
